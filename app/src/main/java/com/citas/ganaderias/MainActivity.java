44573a9ff02a55b641f49ff53931eab13e86483b
CharlieRedDesigner <charleireddesigner@gmail.com>
2022-07-24 00:01:36 +0200

    Prueba 1

--- app/src/main/java/com/citas/ganaderias/MainActivity.java
+++ app/src/main/java/com/citas/ganaderias/MainActivity.java
@@ -3,12 +3,16 @@ package com.citas.ganaderias;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+        private EditText et1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
